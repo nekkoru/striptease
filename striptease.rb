b3b88acf8a018ec87dd5a891c2defc0cb7f64182
Nekkoru <nekkoru@gmail.com>
2013-02-17 19:50:59 +0100

    Model and view

--- striptease.rb
+++ striptease.rb
@@ -3,7 +3,24 @@
 #https://github.com/nekkoru/striptease
 
 require 'sinatra'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/devel.db")
+
+class Strip
+  include DataMapper::Resource
+  property :id,       Serial
+  property :title,    String, :required => true
+  property :posted,   DateTime
+  property :filename, String, :required => true
+end
+
+DataMapper.finalize
 
 get '/' do
-  "Hello, world!"
+  @strips = Strip.all
+  erb :index
 end
+
+
