bfe96ac4a24ada66f0c9ba0ad1cb70bc20db3e68
Nekkoru <nekkoru@gmail.com>
2013-02-17 18:25:24 +0100

    Initial commit.

---
+++ striptease.rb
@@ -0,0 +1,9 @@
+#Striptease - dead simple webcomic client
+#2013 Mathilda Hartnell <nekkoru@gmail.com>
+#https://github.com/nekkoru/striptease
+
+require 'sinatra'
+
+get '/' do
+  "Hello, world!"
+end
