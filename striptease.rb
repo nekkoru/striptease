7c544a8610d747d749bef8ec282d78e8b6419f0d
Nekkoru <nekkoru@gmail.com>
2013-06-09 14:37:09 +0200

    Added hook routes for admin panel, need to finish upload

--- striptease.rb
+++ striptease.rb
@@ -19,14 +19,60 @@ end
 
 DataMapper.finalize
 
+#simple authentication, to be replaced later
+#description of the method should be in the documentation
+set :username, 'admin'
+set :token, SecureRandom.base64(32)
+set :password, 'default'
+
+helpers do
+  def admin?
+    request.cookies[settings.username]==settings.token
+  end
+
+  def protected!
+    halt 401, 'Not authorized' unless admin?
+  end
+end
+
+
+
 get '/' do
   erb :index, :locals => [ :strip => Strip.last, :request => :strip ]
+end
+
+get '/archive' do
+  erb :index, :locals => [ :strips => Strip.all, :request => :list ]
+end
+
+get '/admin' do
+  if admin?
+    redirect '/admin_panel'
+  else
+    erb :admin
+  end
+end
+
+post '/login' do
+  if params['username']==settings.username&&params['password']==settings.password
+    response.set_cookie(settings.username, :value=>settings.token)
+    redirect '/admin_panel'
+  else
+    "Username or password incorrect"
+  end
+end
+
+get '/logout' do
+  response.set_cookie(settings.username, false)
+  redirect '/'
+end
+
+get '/admin_panel'
+  protected!
+  erb :admin_panel
 end
 
 get '/:id' do
   erb :index, :locals => [ :strip => Strip.get(params[:id]), :request => :strip ]
 end
 
-get '/archive' do
-  erb :index, :locals => [ :strips => Strip.all, :request => :list ]
-end
